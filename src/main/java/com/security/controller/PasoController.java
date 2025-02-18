9d0e4bfff43d9c32e43c5bafd6cbd63b365e0879
KevinChiguano <kevin_chili1998@hotmail.com>
2025-02-18 11:01:44 -0500

    add method last step

--- src/main/java/com/security/controller/PasoController.java
+++ src/main/java/com/security/controller/PasoController.java
@@ -1,5 +1,8 @@
 package com.security.controller;
 
+import java.util.Comparator;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -26,41 +29,62 @@ public class PasoController {
     @Autowired
     private IGestorPasoService gestorPasoService;
 
-
-    /*@GetMapping(path = "/{proceso}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> crearPasos(@PathVariable String proceso){
-        return new ResponseEntity<>(this.procesoPasoService.crearPasos(proceso), null, HttpStatus.OK);
-    }*/
+    /*
+     * @GetMapping(path = "/{proceso}", produces = MediaType.APPLICATION_JSON_VALUE)
+     * public ResponseEntity<?> crearPasos(@PathVariable String proceso){
+     * return new ResponseEntity<>(this.procesoPasoService.crearPasos(proceso),
+     * null, HttpStatus.OK);
+     * }
+     */
 
     @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> buscarPorId(@PathVariable(name="id") Integer id){
+    public ResponseEntity<?> buscarPorId(@PathVariable(name = "id") Integer id) {
         return new ResponseEntity<>(this.pasoService.findById(id), null, HttpStatus.OK);
     }
 
     @GetMapping(path = "proceso/{idProceso}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> buscarPasosDTOPorProcesoId(@PathVariable(name="idProceso") Integer idProceso){
+    public ResponseEntity<?> buscarPasosDTOPorProcesoId(@PathVariable(name = "idProceso") Integer idProceso) {
         return new ResponseEntity<>(this.gestorPasoService.findPasosDTOByProcesoId(idProceso), null, HttpStatus.OK);
     }
 
+    @GetMapping(path = "proceso/{idProceso}/ultimo", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> buscarUltimoPasoPorProcesoId(@PathVariable(name = "idProceso") Integer idProceso) {
+        List<PasoDTO> pasos = this.gestorPasoService.findPasosDTOByProcesoId(idProceso);
+
+        if (pasos.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        // Obtener el último basado en el ID más alto
+        PasoDTO ultimoPaso = pasos.stream()
+                .max(Comparator.comparing(PasoDTO::getId))
+                .orElse(null);
+
+        return ResponseEntity.ok(ultimoPaso);
+    }
+
     // @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    // public ResponseEntity<?> actualizarEstadoPorId(@RequestParam Integer idPaso, @RequestParam String estado){
-    //     return new ResponseEntity<>(this.pasoService.updateEstado(idPaso, estado), null, HttpStatus.OK);
+    // public ResponseEntity<?> actualizarEstadoPorId(@RequestParam Integer idPaso,
+    // @RequestParam String estado){
+    // return new ResponseEntity<>(this.pasoService.updateEstado(idPaso, estado),
+    // null, HttpStatus.OK);
     // }
 
-    
     @PutMapping(path = "/{idPaso}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> actualizarPasoById(@PathVariable(name="idPaso") Integer idPaso, @RequestBody PasoDTO pasoDTO){
+    public ResponseEntity<?> actualizarPasoById(@PathVariable(name = "idPaso") Integer idPaso,
+            @RequestBody PasoDTO pasoDTO) {
         return new ResponseEntity<>(this.gestorPasoService.updatePaso(idPaso, pasoDTO), HttpStatus.OK);
     }
-    
+
     @PutMapping(path = "/{idPaso}/{idResponsable}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> actualizarPasoResponsable(@PathVariable(name="idPaso") Integer idPaso, @PathVariable(name="idResponsable") Integer idResponsable){
+    public ResponseEntity<?> actualizarPasoResponsable(@PathVariable(name = "idPaso") Integer idPaso,
+            @PathVariable(name = "idResponsable") Integer idResponsable) {
         return new ResponseEntity<>(this.gestorPasoService.updatePasoResponsable(idPaso, idResponsable), HttpStatus.OK);
     }
 
     @GetMapping(path = "/estados", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> buscarTodosEstados(){
+    public ResponseEntity<?> buscarTodosEstados() {
         return new ResponseEntity<>(this.pasoService.buscarEstados(), HttpStatus.OK);
     }
-    
+
 }
